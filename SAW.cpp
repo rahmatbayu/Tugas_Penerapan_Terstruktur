623f0d890b2878802c19a68d3bf55f7c3c194aed
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2018-01-03 14:16:35 -0800

    mendeklarasikan variable menggunakan array

--- SAW.cpp
+++ SAW.cpp
@@ -4,8 +4,11 @@
 using namespace std;
 
 int main(){
-	// DEKLARASI VARIABEL
-	
+	// DEKLARASI VARIABEL BERDASAKAN ARRAY
+	int i,j,n,b,a[8];
+	double c[i][j], ac[3][5] = {{4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2}};
+	double w[] = {5,3,4,4,2};
+	double hasil1,hasil2,hasil3,hasilkali,mc1,mc2,mc3,mc4,mc5,h1,h2,h3,h4,h5,h11;
 	
 	// SETTING PRESISI DESIMAL
 
