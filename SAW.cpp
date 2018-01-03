55cfccf2134ce2743837c0b8645b0394b1bdef0a
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2018-01-03 14:52:25 -0800

    menampilkan nilai bobot w

--- SAW.cpp
+++ SAW.cpp
@@ -43,7 +43,10 @@ int main(){
 		cout<<endl;
 	}
 	cout << "BOBOT W :"<<endl;
-
+	for (j=0;j<5;j++) {
+			cout<<w[j]<<" | ";			
+}
+	cout<<endl;
 	cout << "MATRIKS R * W :"<<endl;
 
 	cout << "HASIL AKHIR  :"<<endl;
